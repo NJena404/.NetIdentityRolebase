fe3db398c1fd07430332228a0df0d4b978b1f28e
NARAYAN <NARAYAN@DESKTOP-C38JTQ2>
2023-07-24 12:33:36 +0530

    Add project files.

---
+++ AspnetIdentity/Data/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AspnetIdentity.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? Name { get; set; }
+
+        public string? DisplayName { get; set; }
+    }
+}
